3de70a39262c8cff8ee060d1b40f218878c4c6f5
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-04-26 20:32:49 +0200

    remove model, set entity and create annotation to mapping data

--- src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,5 +1,5 @@
 package com.springboot.thymeleafdemo.controller;
-import com.springboot.thymeleafdemo.model.Employee;
+import com.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
