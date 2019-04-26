8b3b68c1fc0687a8f8813d504aa6eeba94e575a0
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-04-26 17:22:10 +0200

    create test hello world app with Thymeleaf

---
+++ src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String sayHello (Model model){
+        model.addAttribute("theDate",new java.util.Date());
+        return "helloworld";
+    }
+}
