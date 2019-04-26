802a4c938d8d35d46d2525fec54c912f7fc81747
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-04-26 19:14:36 +0200

    show the list of employees with bootstrap formatting

---
+++ src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.springboot.thymeleafdemo.controller;
+import com.springboot.thymeleafdemo.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private List<Employee> employees;
+
+    @PostConstruct
+    private void loadData(){
+        employees = new ArrayList<>();
+        employees.add(new Employee(1, "Piotr", "Tomaszewski", "piotr@tomaszew.com"));
+        employees.add(new Employee(2, "Adam", "Tomaszewski", "adamtomkrk@gmail.com"));
+        employees.add(new Employee(3,"Daniel", "Kłusek", "firgasto@gmail.com"));
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+        model.addAttribute("employees",employees);
+        return "list-employees";
+    }
+}
+
