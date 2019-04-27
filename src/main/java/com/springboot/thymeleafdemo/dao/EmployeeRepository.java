f66b0605c4182240c0f98896761bcf415a621b62
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-04-27 16:27:16 +0200

    add ordered data by last name

--- src/main/java/com/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,6 +1,10 @@
 package com.springboot.thymeleafdemo.dao;
-
 import com.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> { }
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByOrderByLastNameAsc();
+}
+
